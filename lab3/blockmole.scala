5e46aacaac19d1696b4f3ef2a385b808c4185e61
SimonHallefelt <simon@hallefelt.se>
2020-09-25 15:16:18 +0200

    lab3 snart klar

--- lab3/blockmole.scala
+++ lab3/blockmole.scala
@@ -12,17 +12,70 @@ object Color {
 }
 
 object Blockwindow {
+    import introprog.PixelWindow
+
+    val windowSize = (30, 50)
+    val blockSize = 10
+
+    val window = new PixelWindow(windowSize._1 * blockSize, windowSize._2 * blockSize, "Spela Blockmole")
+
+    type Pos = (Int, Int)
+
+    def block(pos: Pos)(color: JColor = JColor.gray): Unit = {
+        val x = pos._1 * blockSize
+        val y = pos._2 * blockSize
+        window.fill(x, y, blockSize, blockSize, color) 
+    }
+
+    def rektangel(leftTop: Pos)(size: (Int, Int))(color: JColor = JColor.gray): Unit = {
+        for (y <- leftTop._2 until size._2) {
+            for (x <- leftTop._1 until size._1){
+                block(x, y)(color)
+
+            }
+        }
+    }
+
+    val macWaitMillis = 10
+
+    def waitForKey(): string = {
+        window.awaitEvent(maxWaitMillis)
+        while (window.lastEventType != PixxelWindow.Event.KeyPressed) {
+            window.awaitEvent(maxWaitMillis)
+        }
+        println(s"KeyPressed: " + window.lastkey)
+        window.lastKey
+    }
 
 }
 
 object mole {
-    def dig(): Unit = println("Här ska det grävas!")
+    def dig(): Unit = {
+        println("Här ska det grävas!")
+        var x = Blockwindow.windowSize._1 / 2
+        var y = Blockwindow.windowSize._2 / 2
+        var quit = false
+        while (!quit) {
+            Blockwindow.block(x, y)(Color.mole)
+            val key = Blockwindow.waitForKey()
+            if (key == "w") y -= y
+            else if (key == "a") x -= x
+            else if (key == "s") y += y
+            else if (key == "d") x += x
+            else if (key == "q") quit = true
+
+        }
+    }
 }
 
-
-
 object Main{
-    def drawWorld(): Unit = println("Ska rita ut underjorden!")
+    def drawWorld(): Unit = {
+        println("Ska rita ut underjorden!")
+        Blockwindow.rektangel(0, 0)(Blockwindow.windowSize._1, 10)(Color.sky)
+        Blockwindow.rektangel(0, 10)(Blockwindow.windowSize._1, 20)(Color.grass)
+        Blockwindow.rektangel(0, 20)(Blockwindow.windowSize._1, Blockwindow.windowSize._2)(Color.soil)
+
+    }
 
     def main(args: Array[String]): Unit = {
         drawWorld()
@@ -30,3 +83,16 @@ object Main{
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
