8a9867e92faad833758d1e858f7e41220e5819aa
Simon <simon@hallefelt.se>
2020-10-06 19:13:52 +0200

    spelet fungerar typ

--- lab4/Game.scala
+++ lab4/Game.scala
@@ -1,15 +1,32 @@
 package blockbattle
 
+import java.awt.{Color => JColor}
+
+
 object Game{
     val windowSize = (30,50)
     val windowTitle = "EPIC BLOCK BATTLE"
     val blockSize = 14
     val skyRange = 0 to 7
     val grassRange = 8 to 8
-    object Color {???}
+    object Color {
+        val black = new JColor(0, 0, 0)
+        val mole1 = new JColor(51, 51, 0)
+        val mole2 = new JColor(70, 51, 0)
+        val soil = new JColor(153, 102, 51)
+        val tunnel = new JColor(204, 153, 102)
+        val grass = new JColor(25, 130, 35)
+        val sky = new JColor(0, 0, 100)
+        val worm = new JColor(225, 100, 235) 
+    }
 
     def backgroundCollorAtDepth(y: Int): java.awt.Color = {
-        ???
+//        eraseBlocks()
+        
+        if (skyRange contains(y)) Color.sky
+        else if (grassRange contains(y)) Color.grass
+        else Color.soil
+
     }
 }
 
@@ -20,16 +37,65 @@ class Game(
     import Game._
 
     val window = new BlockWindow(windowSize, windowTitle, blockSize)
-    val leftMole: Mole = ???
-    val rightMole: Mole = ???
+    val leftMole: Mole = {
+        val keyControl = KeyControl(left="a",down="s",right="d",up="w")
+        val pos = Pos(14, 25)
+        Mole(name=leftPlayerName, pos, dir=(-1,0), Color.mole1, keyControl)
+    }
+    val rightMole: Mole = {
+        val keyControl = KeyControl(left="Left",down="Down",right="Right",up="Up")
+        val pos = Pos(16, 26)
+        Mole(name=rightPlayerName, pos, dir=(1,0), Color.mole2, keyControl)
+    }
 
-    def drawWorld(): Unit = ???
+    def drawWorld(): Unit = {
+        for(y <- 0 to windowSize._2){
+            for(x <- 0 to windowSize._1){
+                window.setBlock(Pos(x, y), backgroundCollorAtDepth(y))
+            }
+        }
+        window.setBlock(Pos(15, 15), java.awt.Color.red)
+    }
 
-    def eraseBlocks(x1: Int, y1: Int, x2: Int, y2: Int): Unit = ???
+    def eraseBlocks(x1: Int, y1: Int, x2: Int, y2: Int): Unit = {
+        ???
+    }
 
-    def update(mole: Mole): Unit = ???
+    def handleEvents(): Unit = {
+        var e = window.nextEvent()
+        while(e != BlockWindow.Event.Undefined) {
+            e match {
+                case BlockWindow.Event.KeyPressed(key) =>
+                    leftMole.setDir(key)
+                    rightMole.setDir(key)
 
-    def gameLoop(): Unit = ???
+                case BlockWindow.Event.WindowClosed =>
+                    ???
+            }
+            e = window.nextEvent()
+        }
+    }
+
+    def update(mole: Mole): Unit = {
+        window.setBlock(mole.pos, Color.tunnel)
+        mole.move()
+        window.setBlock(mole.pos, mole.color)
+    }
+
+    var quit = false
+    val delayMillis = 200
+
+    def gameLoop(): Unit = {
+        while(!quit) {
+            val t0 = System.currentTimeMillis
+            handleEvents()
+            update(leftMole)
+            update(rightMole)
+
+            val elapsedMillis = (System.currentTimeMillis - t0).toInt
+            Thread.sleep((delayMillis - elapsedMillis) max 0)
+        }
+    }
 
     def start(): Unit = {
         println("Start digging!")
--- lab4/Main.scala
+++ lab4/Main.scala
@@ -1,28 +1,9 @@
 package blockbattle
 
 object Main{
-    def drawWorld(): Unit = {
-        println("Ska rita ut underjorden!")
-
-    }
-
     def main(args: Array[String]): Unit = {
-        drawWorld()
-
-        import blockbattle._
-
-        val kplayer1 = KeyControl(left="a",down="s",right="d",up="w")
-        val pplayer1 = Pos(1, 1)
-        val player1 = {
-            Mole(name="jag", pos=pplayer1, dir=(0,0), color=java.awt.Color.BLUE, keyControl=kplayer1)
-        }
-
-        import java.awt.Color
-        
-        val windowSize = (30, 40)
-        val bW = BlockWindow(windowSize, "hej", 12)
-
-        bW.setBlock(Pos(2, 2), Color.red)
+        val g = new Game
+        g.start()
 
     }
 
