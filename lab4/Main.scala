07ea1d3c1943827c58e2c1e338ef265a1dffb1d4
Simon Hallefält <71182918+SimonHallefelt@users.noreply.github.com>
2020-10-06 09:38:41 +0200

    öppnar fönstret

--- lab4/Main.scala
+++ lab4/Main.scala
@@ -17,7 +17,13 @@ object Main{
             Mole(name="jag", pos=pplayer1, dir=(0,0), color=java.awt.Color.BLUE, keyControl=kplayer1)
         }
 
+        import java.awt.Color
         
+        val windowSize = (30, 40)
+        val bW = BlockWindow(windowSize, "hej", 12)
+
+        bW.setBlock(Pos(2, 2), Color.red)
+
     }
 
 }
