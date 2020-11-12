6762d34dcb4df73cdf20bcc9806e850f83357424
Simon <simon@hallefelt.se>
2020-11-12 22:59:45 +0100

    början till lab7

---
+++ lab7/FreqMapBuilder.scala
@@ -0,0 +1,17 @@
+package nlp
+
+class FreqMapBuilder{
+    private val register = collection.mutable.Map.empty[String, Int]
+    def toMap: Map[String, Int] = register.toMap
+    def add(s: String): Unit = {
+        register += (s -> (register.getOrElse(s, 0)) + 1)
+    }
+}
+
+object FreqMapBuilder {
+    def apply(xs: String*): FreqMapBuilder = {
+        val resultat = new FreqMapBuilder
+        xs.foreach(resultat.add)
+        resultat
+    }
+}
